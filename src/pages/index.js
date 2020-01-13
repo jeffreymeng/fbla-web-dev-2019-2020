7f64e20be794232ede664d60dec8e821e0b9a235
Jeffrey Meng <jeffkmeng@gmail.com>
2020-01-12 16:14:00 -0800

    setup postcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import React from "react"
+import "../styles/site.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Hello Gatsby!</div>
+)
