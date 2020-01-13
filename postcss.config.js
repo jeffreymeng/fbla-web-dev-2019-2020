6d682f18f4da11728b15282625e3fda03898f06e
Jeffrey Meng <jeffkmeng@gmail.com>
2020-01-12 16:26:41 -0800

    fix css

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
   plugins: [
-    purgecss({
-      content: ['./**/*.html']
-    }),
     require("tailwindcss")
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from "react"
 import "../styles/site.css"
 
 export default () => (
-    <div class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Hello Gatsby!</div>
+    <div class="bg-blue-500 hover:bg-blue-400 border-b-4 hover:border-b-0 text-white font-bold py-2 px-4  border-blue-700 hover:border-blue-500 rounded">Hello Gatsby!</div>
 )
