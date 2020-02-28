dbe6f0ddddf8a6477e059505bb578a9e86a7619d
Nathan Wang <nathan.r.wang@gmail.com>
2020-02-27 18:30:35 -0800

    finish jobs.... for now

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -17,36 +17,74 @@ const JobsPage = () => (
     </div>
 
     <div className="job-benefits" id="benefits">
-      <h2 className="job-benefits__heading">Why Us?</h2>
-      <div className="job-benefits__list">
-        <div className="job-benefits__list__item">
-          <div className="job-benefits__list__item__number">
+      <h2 className="job__section-heading">Why Us?</h2>
+      <div className="numbered-sections-list">
+        <div className="numbered-sections-list__item">
+          <div className="numbered-sections-list__item__number">
             01
           </div>
-          <div className="job-benefits__list__item__content">
-            <h3 className="job-benefits__list__item__content__title">Travel the World</h3>
+          <div className="numbered-sections-list__item__content">
+            <h3 className="numbered-sections-list__item__content__title">Travel the World</h3>
             When working for Coastal Airlines, you can travel the world -- and get paid to do it!
             Visit Hawaii, Rome, Paris, and so much more with Coastal Airlines.
           </div>
         </div>
-        <div className="job-benefits__list__item">
-          <div className="job-benefits__list__item__number">
+        <div className="numbered-sections-list__item">
+          <div className="numbered-sections-list__item__number">
             02
           </div>
-          <div className="job-benefits__list__item__content">
-            <h3 className="job-benefits__list__item__content__title">Comprehensive Employee Benefits</h3>
+          <div className="numbered-sections-list__item__content">
+            <h3 className="numbered-sections-list__item__content__title">Comprehensive Employee Benefits</h3>
             Coastal Airlines provides extremely comprehensive employee benefits to all employees.
             We provide renowned benefits including healthcare, 401(k) matching, and much more!
           </div>
         </div>
-        <div className="job-benefits__list__item">
-          <div className="job-benefits__list__item__number">
+        <div className="numbered-sections-list__item">
+          <div className="numbered-sections-list__item__number">
             03
           </div>
-          <div className="job-benefits__list__item__content">
-            <h3 className="job-benefits__list__item__content__title">Proven Employee Testimonials</h3>
+          <div className="numbered-sections-list__item__content">
+            <h3 className="numbered-sections-list__item__content__title">Proven Employee Testimonials</h3>
             We have more than 500 satisfied employees! We are consistently ranked amongst
             the top airlines to work for.
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    {/* TODO add testimonial */}
+
+    <div className="job-apply__container" id="apply">
+      <div className="job-apply">
+        <h2 className="job__section-heading">Apply Now</h2>
+        <div className="numbered-sections-list numbered-sections-list--green">
+          <div className="numbered-sections-list__item">
+            <div className="numbered-sections-list__item__number">
+              01
+            </div>
+            <div className="numbered-sections-list__item__content">
+              <h3 className="numbered-sections-list__item__content__title">Select a Position</h3>
+              Choose from a variety of different job openings! <a href="#">A complete list can be found here.</a>
+            </div>
+          </div>
+          <div className="numbered-sections-list__item">
+            <div className="numbered-sections-list__item__number">
+              02
+            </div>
+            <div className="numbered-sections-list__item__content">
+              <h3 className="numbered-sections-list__item__content__title">Send Your Resume</h3>
+              Make a resume and cover letter for your selected position and
+              send it via e-mail to <a href="mailto:jobs@coastalairlines.com">jobs@coastalairlines.com</a>.
+            </div>
+          </div>
+          <div className="numbered-sections-list__item">
+            <div className="numbered-sections-list__item__number">
+              03
+            </div>
+            <div className="numbered-sections-list__item__content">
+              <h3 className="numbered-sections-list__item__content__title">Apply!</h3>
+              We'll get back to you within a few days to arrange an interview!
+            </div>
           </div>
         </div>
       </div>
