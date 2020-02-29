4d51d3079bd8d365392f113744acbc07c765b499
Nathan Wang <nathan.r.wang@gmail.com>
2020-02-29 13:03:02 -0800

    it's beautiful

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,50 +5,47 @@ import SEO from "../components/seo"
 import "../styles/index.scss"
 import { graphql, StaticQuery, navigate } from "gatsby"
 import BookingForm from "../components/BookingForm";
-import bgImage from "../images/bg.jpg"
+import Img from "gatsby-image"
 
-const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        desktop: file(
-          relativePath: { eq: "bg.jpg" }
-        ) {
-          childImageSharp {
-            fluid(quality: 100, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Layout
-        // pageInfo={{ pageName: "index" }}
-        // lightFooter
-      >
-        <SEO
-          title="Home"
-        />
+const IndexPage = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout
+      // pageInfo={{ pageName: "index" }}
+      // lightFooter
+    >
+      <SEO
+        title="Home"
+      />
 
-        <div className="pt-32 pb-12 relative">
-          <div className="absolute top-0 bottom-0 left-0 right-0 bg-cover bg-center -z-10" style={{
-            backgroundImage: `url('${bgImage}')`,
-            backgroundColor: "blue",
-            filter: "contrast(70%)",
-            backgroundBlendMode: "multiply",
-          }}/>
-          <h1 className="px-4 text-center text-white text-4xl tracking-tight leading-10 font-extrabold text-gray-50 sm:text-5xl sm:leading-none md:text-6xl">Where will we be flying today?</h1>
-
-          <BookingForm onSubmit={(data) => navigate("otherpage", {
-            state:data
-          })} dropUp className="relative" style={{
-            bottom: "-7rem",
-          }} />
+      <div className="pt-32 pb-12 relative">
+        <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden -z-10">
+          <Img className="max-h-full" fluid={data.file.childImageSharp.fluid} />
         </div>
-      </Layout>
-    )}
-  />
-)
+        <h1 className="px-4 text-center text-white text-4xl tracking-tight leading-10 font-extrabold text-gray-50 sm:text-5xl sm:leading-none md:text-6xl">Where will we be flying today?</h1>
+
+        <BookingForm onSubmit={(data) => navigate("otherpage", {
+          state:data
+        })} dropUp className="relative" style={{
+          bottom: "-7rem",
+        }} />
+      </div>
+    </Layout>
+  )
+};
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "bg_dark.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 1920, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
