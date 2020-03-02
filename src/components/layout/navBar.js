abe88f55a13413cafcd55aa0ad7140cd6a198aad
Nathan Wang <nathan.r.wang@gmail.com>
2020-03-01 19:56:15 -0800

    my flights

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -1,16 +1,33 @@
-import React, { useContext, useState } from "react"
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react"
 import { Link } from "gatsby"
 import classNames from "classnames"
 import links from "./navlinks";
 
 import invisLogo from "../../images/longCoastalInvisV2.png"
 import AuthContext from "../../context/AuthContext"
+import Transition from "../Transition"
 
 
 const CustomNavbar = ({ pageInfo }) => {
   const auth = useContext(AuthContext);
 
-  const [open, setOpen] = useState(false)
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  const [userNavOpen, setUserNavOpen] = useState(false);
+  const userDropdownRef = useRef();
+
+  const onClick = useCallback(e => {
+    if (userNavOpen && userDropdownRef.current.contains(e.target)) {
+      return;
+    }
+    setUserNavOpen(false);
+  }, [userDropdownRef]);
+
+  useEffect(() => {
+    if (document != null) {
+      document.addEventListener("mousedown", onClick, false);
+    }
+    return () => document.removeEventListener("mousedown", onClick, false);
+  }, []);
 
   return (
     <nav className="bg-gray-800">
@@ -18,20 +35,20 @@ const CustomNavbar = ({ pageInfo }) => {
         <div className="relative flex items-center h-16">
           <div className="flex items-center md:hidden">
             <button
-              onClick={() => setOpen(!open)}
+              onClick={() => setMobileNavOpen(!mobileNavOpen)}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out">
               <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path
                   className={classNames({
-                    "hidden": open,
-                    "inline-flex": !open,
+                    "hidden": mobileNavOpen,
+                    "inline-flex": !mobileNavOpen,
                   })}
                   strokeLinecap="round"
                   strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
                 <path
                   className={classNames({
-                    "hidden": !open,
-                    "inline-flex": open,
+                    "hidden": !mobileNavOpen,
+                    "inline-flex": mobileNavOpen,
                   })}
                   strokeLinecap="round"
                   strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
@@ -61,21 +78,49 @@ const CustomNavbar = ({ pageInfo }) => {
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {!auth.user && <Link to="/sign-in"
-                                className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
-              Sign In
-            </Link>}
-            {auth.user && <button onClick={() => auth.signOut()}
-                               className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
-              Sign Out
-            </button>}
-          </div>
+
+          {
+            auth.user &&
+            <div className="mx-4 flex-shrink-0 flex items-center">
+              <div ref={userDropdownRef} className="ml-3 relative">
+                <div>
+                  <button onClick={() => setUserNavOpen(!userNavOpen)} className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                    <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                  </button>
+                </div>
+                <Transition
+                  show={userNavOpen}
+                  enter="transition ease-out duration-200"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterEnd="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                  className="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg">
+                  <div className="py-1 rounded-md bg-white shadow-xs">
+                    <Link to="my-flights" className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">My Flights</Link>
+                    <a href="#" onClick={(e) => {e.preventDefault();auth.signOut()}} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Sign out</a>
+                  </div>
+                </Transition>
+              </div>
+            </div>
+          }
+
+          {
+            !auth.user &&
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <Link to="/sign-in"
+                    className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
+                Sign In
+              </Link>
+            </div>
+          }
         </div>
       </div>
+
       <div className={classNames({
-        "block": open,
-        "hidden": !open,
+        "block": mobileNavOpen,
+        "hidden": !mobileNavOpen,
       }, "md:hidden")}>
         <div className="px-2 pt-2 pb-3">
           {links.map((link, idx) => (
