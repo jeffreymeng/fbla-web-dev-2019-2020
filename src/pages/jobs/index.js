6acd74b53ec1d35aaef6f9069cdb50ce6dd18b2c
SavageFish <nisgreat1@gmail.com>
2020-03-01 21:58:09 -0800

    idek?

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -4,12 +4,12 @@ import { Link } from "gatsby";
 import SEO from "../../components/seo"
 import Layout from "../../components/layout/layout"
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-
+import flightJob from "../../images/flightJob.jpg"
 
 const JobsPage = () => (
   <Layout parallax={"jobs"}>
     <SEO title="Apply for a Job" />
-
+{/*}
     <div className="job-hero py-12 md:py-16 lg:py-24">
       <h1 className="job-hero__title mb-6 lg:mb-8 text-4xl md:text-5xl lg:text-6xl xl:text-8xl">Looking for a <span className="job-hero__title__emphasis">Job</span>?</h1>
       <p className="job-hero__subtitle text-2xl md:text-3xl lg:text-4xl xl:text-4xl">Travel the world for a living at Coastal Airlines!</p>
@@ -24,6 +24,120 @@ const JobsPage = () => (
               to="jobs/apply">Apply Now</Link>
       </div>
     </div>
+
+                Look no further. With the right steps, you can start travelling the world for a living at Coastal Airlines!
+
+  <div class="relative bg-white overflow-hidden">
+  <div class="hidden lg:block lg:absolute lg:inset-0">
+    <svg class="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8" width="640" height="784" fill="none" viewBox="0 0 640 784">
+      <defs>
+        <pattern id="svg-pattern-squares-desktop" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+      <rect x="118" width="404" height="784" fill="url(#svg-pattern-squares-desktop)" />
+    </svg>
+  </div>
+  
+    <div class="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
+      <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+        <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+          {/*<div class="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+            Coming soon
+          </div>
+          <h2 class="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
+            Looking for a
+            <br class="hidden md:inline" />
+            <span class="text-indigo-600">Job</span> ?
+          </h2>
+          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+            Look no further. With the right steps, you can start travelling the world for a living at Coastal Airlines!
+          </p>
+           <div class="mx-15 md:mx-0 mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start  pt-10 sm:pt-10 md:pt-12 lg:pt-16 xl:pt-20">
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <AnchorLink href="#Section1" offset="45" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 ">
+                Learn More
+              </AnchorLink>
+            </div>
+          </div>
+         </div>
+        <div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+          <svg class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden" width="640" height="784" fill="none" viewBox="0 0 640 784">
+            <defs>
+              <pattern id="svg-pattern-squares-mobile" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+            <rect x="118" width="404" height="784" fill="url(#svg-pattern-squares-mobile)" />
+          </svg>
+          <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+            <button class="relative block w-full rounded-lg overflow-hidden focus:outline-none focus:shadow-outline">
+              <img class="w-full" src={flightJob} alt="Woman making a sale" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+*/}
+  
+
+
+<div class="relative bg-white overflow-hidden">
+  <div class="hidden lg:block lg:absolute lg:inset-0">
+    <svg class="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8" width="640" height="784" fill="none" viewBox="0 0 640 784">
+      <defs>
+        <pattern id="svg-pattern-squares-desktop" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+      <rect x="118" width="404" height="784" fill="url(#svg-pattern-squares-desktop)" />
+    </svg>
+  </div>
+  <div class="relative pt-6 pb-16 md:pb-20 lg:pb-24 xl:pb-32">
+    <div class="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
+      <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+        <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+          <div class="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+            Welcome
+          </div>
+          <h2 class="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
+            Looking for a
+            <br class="hidden md:inline" />
+            <span class="text-indigo-600"> Job</span> ?
+          </h2>
+          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+            Look no further. With the right steps, you can start travelling the world for a living at Coastal Airlines!
+          </p>
+         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <AnchorLink href="#benefits" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Learn More
+              </AnchorLink>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <Link to="jobs/apply" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Apply Now
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+          <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+            <button class="relative block w-full rounded-lg overflow-hidden focus:outline-none focus:shadow-outline">
+              <img class="w-full" src={flightJob} alt="Woman making a sale" />
+             
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
     <div className="bg-gray-50 pt-12 pb-8">
       <div className="job-benefits" id="benefits">
