dd5b4a5dcc3c6adb2f93730b4e12a3092fa6504e
Nathan Wang <nathan.r.wang@gmail.com>
2020-03-03 18:40:47 -0800

    fix netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         name: `Troop 485`,
         short_name: `T485`,
         start_url: `/`,
-        icon: "src/images/logo144.png", // TODO
+        icon: "src/images/logo144.png",
         background_color: `#20232a`,
         theme_color: `#20232a`,
       }
